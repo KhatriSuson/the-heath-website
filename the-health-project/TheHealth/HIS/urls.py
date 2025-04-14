28e88974b73f02f99da76e8d5a4cdb43e2519de3
Sushan Khatri <109749923+KhatriSuson@users.noreply.github.com>
2025-04-14 19:00:14 +0545

    initial commit

--- the-health-project/TheHealth/HIS/urls.py
+++ the-health-project/TheHealth/HIS/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home, adminLogin, createID
+from .views import home, adminLogin, createID, index
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('admin-login', adminLogin, name='admin-login'),
+    path('', index, name='index'),
+    path('admin-login', adminLogin, name='admin-login'),  
     path('create-id', createID, name='create-id'),
 ]
