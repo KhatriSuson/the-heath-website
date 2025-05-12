dbc063ae9176a3f07b6b8992e2d6b3a8eb41b988
Sushan Khatri <109749923+KhatriSuson@users.noreply.github.com>
2025-05-12 12:48:53 +0545

    integrate the django project

--- the-health-project/TheHealth/HIS/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CreateID,Patient, Doctor
-# Register your models here.
-
-admin.site.register(Patient)
-admin.site.register(Doctor)
-admin.site.register(CreateID)
--- the-health-project/TheHealth/HIS/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home, adminLogin, createID, index
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('admin-login', adminLogin, name='admin-login'),  
-    path('create-id', createID, name='create-id'),
-]
